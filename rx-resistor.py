c93eecc57e638e8d5e32726893f068bd86c75358
LionyxML <rahul.juliato@gmail.com>
2021-06-15 00:36:28 -0300

    Removed unusefull comments. PEP8 all document.

--- rx-resistor.py
+++ rx-resistor.py
@@ -1,21 +1,9 @@
-#!/usr/bin/python3
-# Arquivo : rresistorX.py
-# Programa: Treino de leitura de resistores
-# Autor   : Rahul Martim Juliato
-# Versão  : 0.1  -  24.10.2018
-# Versão  : 0.2  -  29.12.2019 - Nome mudado de rresistorX para rx-resistor
-
-
-# ---===[0. Bibliotecas]===---
 import tkinter as tk
 from tkinter import messagebox as mb
 from tkinter import ttk
-import math
-
-# ---===[0. Fim das Bibliotecas]===---
+from math import floor, log10
 
 
-# ---===[1. Funções]===---
 def quit():
     """ Sai do programa destruindo o necessário
     """
@@ -50,10 +38,6 @@ def erro(mensagem):
     mb.showerror("Erro!", mensagem)
 
 
-# Snipet para conversão em números de engenharia
-from math import floor, log10
-
-
 def powerise10(x):
     """ Returns x as a*10**b with 0 <= a < 10
     """
@@ -77,9 +61,6 @@ def eng(x):
     a = a * 10 ** (b % 3)
     b = b - b % 3
     return "%.4gE%s" % (a, b)
-
-
-## Fim do Snipet
 
 
 def calcula():
@@ -238,19 +219,12 @@ def desenha_botoes():
         ii += 1
 
 
-# ---===[1. Fim das Funções]===---
-
-
-# ---===[2. Início da geração da Janela]===---
-# 2.0. Definições principais da janela
 janela = tk.Tk()
 janela.wm_title("rx-resistor v0.2")
 janela.wm_minsize(600, 220)
 janela.grid_anchor(anchor="c")
-# janela.tk_setPalette('white')
 
 
-# 2.0. Barra de menu
 barramenu = tk.Menu(janela)
 arquivo = tk.Menu(barramenu, tearoff=800)
 arquivo.add_command(label="Sobre", command=sobre)
@@ -260,21 +234,12 @@ barramenu.add_cascade(label="Arquivo", menu=arquivo)
 
 janela.config(menu=barramenu)
 
-
-# 2.0. Apresentaçã da tela principal
-
-
-# Base para alinhamento do grid
 inicio_row = 0
 inicio_col = 1
 
-
-# Desenha os labels
 lab_faixas = tk.Label(janela, text="Faixas :")
 lab_faixas.grid(sticky="E", row=inicio_row, column=inicio_col - 1)
 
-
-# Radio Button para seleção da quantidade de faixas
 opcao = tk.IntVar()
 opcao.set(1)
 
@@ -284,8 +249,6 @@ rad_opf.grid(column=inicio_col, row=inicio_row, padx=4)
 rad_opf = tk.Radiobutton(janela, text="5", variable=opcao, value=2, command=selec_faixa)
 rad_opf.grid(column=inicio_col + 1, row=inicio_row, padx=4)
 
-
-# Dicionário de definições dos valores padrão de cada faixa em cada tipo de resistor
 valores_4_faixas = [
     {
         "Marrom": 1,
@@ -398,33 +361,23 @@ valores_5_faixas = [
     },
 ]
 
-
-# Seta o valor inicial para o resistor padrão e o modo de 4 faixas
 valores_padrao = valores_4_faixas
 modo = 4
 
-# Cria a variável que irá conter os objetos Entrys onde ocorrerá a seleção
 faixas = {}
 
-# Cria a variável que irá conter a seleção de cada objeto Entry
 selecionado = [tk.StringVar() for x in range(6)]
 
-# Inicia o programa com o desenho demo e desenhando as entrys
 desenha(1)
 desenha_botoes()
 
-# Botão para chamar a função que desenha o resistor
 but_desenha = tk.Button(janela, text="Desenha", command=desenha, width=10)
 but_desenha.grid(sticky="EW", row=8, column=0, columnspan=3, pady=10)
 
-# Botão para calcular o valor ôhmico do resistor
 but_calcula = tk.Button(janela, text="Calcula", command=calcula, width=10)
 but_calcula.grid(sticky="EW", row=9, column=0, columnspan=3)
 
-# Entry para a saída do cálculo
 ent_resultado = tk.Entry(janela, width=40)
 ent_resultado.grid(row=9, column=3)
 
-
 tk.mainloop()
-# ---===[2 Fim da Geração da Janela]===---
